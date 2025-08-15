daf1e56911dfa08b1eb087dc80a0a5bb0cef04f7
Alejo Gustavo Francomano <alejogustavof@gmail.com>
2025-08-15 18:04:18 +0200

    add spinner

--- src/components/LoadingComponent/LoadingComponent.tsx
+++ src/components/LoadingComponent/LoadingComponent.tsx
@@ -1,11 +1,10 @@
-
 function LoadingComponent() {
-
   return (
-     <>
-      Is Loading....
-     </>
-  )
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-white">
+      <img src="/spinner.svg" alt="Loading..." className="w-16 h-16 mb-4 animate-spin" />
+      <label className="text-gray-500 text-lg">Loading...</label>
+    </div>
+  );
 }
 
-export default LoadingComponent
+export default LoadingComponent;
