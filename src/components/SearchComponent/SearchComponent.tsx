04f6c45d9a7eef254ffa848c63dcfdd6883e67fc
Alejo Gustavo Francomano <alejogustavof@gmail.com>
2025-08-17 10:10:55 +0200

    refactor clear search btn

--- src/components/SearchComponent/SearchComponent.tsx
+++ src/components/SearchComponent/SearchComponent.tsx
@@ -11,6 +11,10 @@ const [inputValue, setInputValue] = useState("");
    handleSearch(inputValue);
   };
 
+  const onClear = () => {
+  setInputValue("");
+  clearSearch();
+};
   return (
     <div className="bg-gray-800 text-white p-4 w-full flex flex-row flex-nowrap justify-between">
       <nav className="p-4 bg-gray-800 text-white flex gap-4">
@@ -20,7 +24,7 @@ const [inputValue, setInputValue] = useState("");
       <div className="flex items-center gap-3">
         <input className="border border-gray-300 p-2 rounded" name="title" type="text" placeholder="Search by title..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
         <button onClick={onSearch} className="bg-blue-500 hover:bg-blue-700 transition-all duration-300 ease-in-out text-white px-4 py-2 rounded">Search</button>
-        <button onClick={clearSearch} className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400 transition-all duration-300 ease-in-out">Clear</button>
+        <button onClick={onClear} className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400 transition-all duration-300 ease-in-out">Clear</button>
       </div>
     </div>
   );
