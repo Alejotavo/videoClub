0f6241065440a8c5e0dacae87e5751456f3814df
Alejo Gustavo Francomano <alejogustavof@gmail.com>
2025-08-18 16:26:58 +0200

    add sort on teh table

--- src/components/MovieComponent/MovieList.tsx
+++ src/components/MovieComponent/MovieList.tsx
@@ -3,10 +3,12 @@ import { useFavorites } from "../../contexts/favoritesContext";
 import LoadingComponent from "../LoadingComponent/LoadingComponent";
 import MovieComponent from "./Movie";
 import ErrorComponent from "../ErrorComponent/ErrorComponent";
+import { useState } from "react";
 
 function MovieList() {
   const { titles, loading, error} = useMovie();
   const { addFavorite, removeFavorite, isFavorite } = useFavorites(); 
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
   if (loading) {
       return <LoadingComponent />;
@@ -24,13 +26,27 @@ function MovieList() {
     );
   } 
 
+  // Sort titles based on the year in the specified order
+  const sortedTitles = [...titles].sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.year - b.year;
+    } else {
+      return b.year - a.year;
+    }
+  });
+
   return (
     <>
       <table className="min-w-full bg-white">
         <thead>
           <tr>
             <th className="py-2 px-4 border-b">Title</th>
-            <th className="py-2 px-4 border-b">Year</th>
+            <th className="py-2 px-4 border-b">
+              Year
+              <button onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}>
+                {sortOrder === "asc" ? "🔼" : "🔽"}
+              </button>
+            </th>
             <th className="py-2 px-4 border-b">Rank</th>
             <th className="py-2 px-4 border-b">Actors</th>
             <th className="py-2 px-4 border-b">Poster</th>
@@ -38,7 +54,7 @@ function MovieList() {
           </tr>
         </thead>
         <tbody>
-          {titles.map(t => <MovieComponent  key={t.imdbId} movie={t}  addFavorite={addFavorite} removeFavorite={removeFavorite} isFavorite={isFavorite} />)}
+          {sortedTitles.map(t => <MovieComponent  key={t.imdbId} movie={t}  addFavorite={addFavorite} removeFavorite={removeFavorite} isFavorite={isFavorite} />)}
         </tbody>
       </table>
     </>
